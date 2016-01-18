68671df6a359c8d7fbfb67d6e62ed2242b86ac44
Dylan Hunt <dylanhunt@Dylans-MacBook-Pro.local>
2016-01-18 18:35:06 -0500

    adding templating, cleaning up code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   class Entry
@@ -12,19 +10,19 @@ class ApplicationController < ActionController::Base
   attr_reader :link
 end
 
-def scrape_reddit
-  require 'open-uri'
-  doc = Nokogiri::HTML(open("https://www.reddit.com/r/Showerthoughts/new"))
+  def scrape_reddit
+    require 'open-uri'
+    doc = Nokogiri::HTML(open("https://www.reddit.com/r/Showerthoughts/new"))
 
-  entries = doc.css('.entry')
-  @entriesArray = []
-  entries.each do |entry|
-    title = entry.css('p.title>a').text
-    link = entry.css('p.title>a')[0]['href']
-    @entriesArray << Entry.new(title, link)
+    entries = doc.css('.entry')
+    @redditscraping = []
+    entries.each do |entry|
+      title = entry.css('p.title>a').text
+      link = entry.css('p.title>a')[0]['href']
+      @redditscraping << Entry.new(title, link)
   end
 
-  render template: 'scrape_reddit'
-end
+    render template: 'scrape_reddit'
+  end
 
 end
